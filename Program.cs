1c8b8811e324618f84e88f3f7daa185db875cc41
Pedrolustosa <pedroeternalss@gmail.com>
2021-02-19 22:08:25 -0300

    Calculator - Sum

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Console.Clear();
+            Console.WriteLine("Bem-Vindo");
+            Console.WriteLine("Primeiro Valor");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo valor Valor");
+            float v2 = float.Parse(Console.ReadLine());
 
+            float resultado = v1 + v2;
+            Console.WriteLine("O resultado da soma é: " + resultado);
+            Console.WriteLine($"O resultado da soma é: {resultado}");
+            Console.WriteLine($"O resultado da soma é: {v1 + v2}");
+            Console.WriteLine("O resultado da soma é: " + (v1 + v2));
         }
     }
 }
